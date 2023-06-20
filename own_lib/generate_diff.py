774e67389ab6628ff1df6102db93d2c34f27d4d9
EvgenArt <evgniy.artamonov@yahoo.com>
2023-06-20 22:27:44 +0300

    4_step

--- own_lib/generate_diff.py
+++ own_lib/generate_diff.py
@@ -1,43 +1,4 @@
-# import json
-
-
-# def generate_diff(first_file, second_file):
-#     with open(first_file) as f:
-#         data1 = json.load(f)
-#     with open(second_file) as f:
-#         data2 = json.load(f)
-
-#     keys = set(data1)
-#     keys.update(data2)
-#     print('{')
-#     diff_data = {}
-
-#     for key in keys:
-#         key_d1 = key in data1
-#         key_d2 = key in data2
-
-#         if key_d1 and key_d2:
-#             v1 = data1[key]
-#             v2 = data2[key]
-
-#             if v1 == v2:
-#                 diff_data[f'  {key}'] = v1
-#             else:
-#                 diff_data[f'- {key}'] = v1
-#                 diff_data[f'+ {key}'] = v2
-#         elif key_d1:
-#             diff_data[f'- {key}'] = data1[key]
-#         else:
-#             diff_data[f'+ {key}'] = data2[key]
-
-#     sortdata = dict(sorted(diff_data.items(), key=lambda x: x[0][2]))
-#     for key in sortdata:
-#         print("    " + key, ':', sortdata[key])
-#     print("}")
-#     return ' '
 import json
-
-
 
 
 def generate_diff(file_path1, file_path2):
@@ -53,11 +14,11 @@ def generate_diff(file_path1, file_path2):
     key_set_file2 = set(file_2.keys())
 
     key_remote = key_set_file1 - key_set_file2
-    
+
     key_add = key_set_file2 - key_set_file1
-    
+
     key_same = key_set_file1 & key_set_file2
-    
+
     diff += list(map(lambda x: f'  - {x}: {file_1[x]}', key_remote))
     diff += list(map(lambda x: f'  + {x}: {file_2[x]}', key_add))
     for item in key_same:
