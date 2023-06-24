c539d53e35afd82f82ed4352c44484c068250d59
EvgenArt <evgniy.artamonov@yahoo.com>
2023-06-24 14:12:55 +0300

    5_step

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -2,6 +2,7 @@ import json
 import yaml
 from yaml import SafeLoader
 
+
 def parser(file1, file2):
     if file1.split('.')[1] == 'json':
         file_1 = json.load(open(file1))
@@ -10,7 +11,3 @@ def parser(file1, file2):
         file_1 = yaml.load(open(file1), Loader=SafeLoader)
         file_2 = yaml.load(open(file2), Loader=SafeLoader)
     return file_1, file_2
-
-
-
-
