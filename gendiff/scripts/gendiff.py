680ff1ce26ca2d91d75f45563355671f759e5566
EvgenArt <evgniy.artamonov@yahoo.com>
2023-07-18 08:50:33 +0300

    test_refactor_step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,18 +1,18 @@
-import argparse
+from gendiff.cli import get_args
 from gendiff.diff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
-    )
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-    parser.add_argument(
-        '-f', '--format', help='set format of output'
-    )
-    args = parser.parse_args()
-    print(args.format)
+    # parser = argparse.ArgumentParser(
+    #     description='Compares two configuration files and shows a difference.'
+    # )
+    # parser.add_argument('first_file', type=str)
+    # parser.add_argument('second_file', type=str)
+    # parser.add_argument(
+    #     '-f', '--format', help='set format of output'
+    # )
+    args = get_args()
+    # print(args.format)
     diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
 
